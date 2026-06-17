9088338f0261c8ec60f6247f7421f5ebcce62912
Anmol Kumar <anmol46645@gmail.com>
2026-06-17 18:33:59 +0530

    Fixed root route for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
 import Loans from "./pages/Loans";
@@ -7,55 +7,63 @@ import ApplyLoan from "./pages/ApplyLoan";
 import Profile from "./pages/Profile";
 import Admin from "./pages/Admin";
 import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
     <Routes>
-     <Route
-  path="/dashboard"
-  element={
-    <ProtectedRoute>
-      <Dashboard />
-    </ProtectedRoute>
-  }
-/>
 
-<Route
-  path="/loans"
-  element={
-    <ProtectedRoute>
-      <Loans />
-    </ProtectedRoute>
-  }
-/>
+      <Route
+        path="/"
+        element={<Navigate to="/login" />}
+      />
 
-<Route
-  path="/apply-loan"
-  element={
-    <ProtectedRoute>
-      <ApplyLoan />
-    </ProtectedRoute>
-  }
-/>
+      <Route path="/login" element={<Login />} />
 
-<Route
-  path="/profile"
-  element={
-    <ProtectedRoute>
-      <Profile />
-    </ProtectedRoute>
-  }
-/>
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
 
-<Route
-  path="/admin"
-  element={
-    <ProtectedRoute>
-      <Admin />
-    </ProtectedRoute>
-  }
-/>
+      <Route
+        path="/loans"
+        element={
+          <ProtectedRoute>
+            <Loans />
+          </ProtectedRoute>
+        }
+      />
 
-<Route path="/login" element={<Login />} />
+      <Route
+        path="/apply-loan"
+        element={
+          <ProtectedRoute>
+            <ApplyLoan />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute>
+            <Admin />
+          </ProtectedRoute>
+        }
+      />
+      
     </Routes>
   );
 }
